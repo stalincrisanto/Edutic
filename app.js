504031aab92fbaebc1b8ae319d9e171ff88f8d50
stalincrisanto <stalincrisanto@hotmail.com>
2020-09-15 17:22:30 -0500

    log

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+var cors = require('cors')
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -9,8 +11,15 @@ app.get("/",(req,res)=>{
     res.json({message:"EDUTIC"});
 });
 
-require("../proyecto/routes/rutas.js")(app);
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin','X-Requested-With,content-type,Accept');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
+require("../backend/routes/rutas.js")(app);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log("SERVIDOR INICIADO");
